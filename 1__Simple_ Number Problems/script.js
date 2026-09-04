dcd9cdcbda3db8c6b7eb5c84b5bb092f75593a36
krishna <krish23x@gmail.com>
2026-09-05 05:21:10 +0530

    Initial commit

---
+++ 1__Simple_ Number Problems/script.js
@@ -0,0 +1,100 @@
+//q1
+
+function printNumbersFromOne(n){
+  for(let i=1;i<=n;i++){
+    console.log(i);
+  }
+}
+
+//q2
+
+function printNumberTo1(n){
+  for(let i=n;i>=1;i--){
+    console.log(i);
+  }
+}
+
+// q3
+
+function printEvenNumberToN(n){
+  for(let i=1;i<=n;i++){
+    if(i%2===0){
+      console.log(i);
+    }
+  }
+}
+
+// q4
+
+function sumOfFirstNNaturalNumbers(n){
+  let sum = 0
+  for(let i=1;i<=n;i++){
+    sum+=i
+  }
+  return sum
+}
+
+//q5
+
+function factorialOfN(n){
+  let factorial = 1
+  for(let i=n;i>=1;i--){
+    factorial*=i
+  }
+  return factorial
+}
+
+//q6 
+
+function sumOfAllEvenNumbersUptoN(n){
+  let sum = 0
+  for(let i=1;i<=n;i++){
+    if(i%2===0){
+      sum+=i
+    }
+  }
+  return sum
+}
+
+//q7
+
+function printSquaresOfNumbersFrom1toN(n){
+  for(let i=1;i<=n;i++){
+    console.log(i**2);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
