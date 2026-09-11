f7817bf800c4e6c02f1ab5137bd565ab3324ba27
krishna <krishna.yadav23x@gmail.com>
2026-09-11 07:46:30 +0530

    solved qns

--- 5__Solving decimal numbers 2/script.js
+++ 5__Solving decimal numbers 2/script.js
@@ -7,3 +7,62 @@ function splitNumberToDigits(num){
   }
   return result.reverse()
 }
+
+function removeDecimalPoint(n){
+  let number = n
+  while (number % 1 !== 0) {
+    number *= 10;
+  }
+  return number
+}
+
+function separateWholeAndFractionalPart(n){
+  const fractionalPart = +(n%1).toFixed(2)
+  const wholePart = n-fractionalPart
+  return {fractionalPart,wholePart}
+}
+
+function generateDecimalNumber(whole,fraction){
+  let number = [...whole,...fraction]
+  let output = 0
+  for(let i=0; i<number.length;i++){
+    output+=number[i]*10**(number.length-i-1)
+  }
+  return output/(10**fraction.length)
+}
+
+function reverseNumber(num){
+  let number = num
+  let numArr = []
+  let output = 0
+  while(number>0){
+    numArr.push(number%10)
+    number = Math.floor(number/10)
+  }
+  for(let i=0; i<numArr.length;i++){
+    output+=numArr[i]*10**(numArr.length-i-1)
+  }
+  return output
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
