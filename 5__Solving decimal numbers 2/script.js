89a7eaf5325271d0563b1be96bc554a7035b971b
krishna <krishna.yadav23x@gmail.com>
2026-09-12 06:07:07 +0530

    solved 6 more

--- 5__Solving decimal numbers 2/script.js
+++ 5__Solving decimal numbers 2/script.js
@@ -54,3 +54,86 @@ function isPalindrome(n) {
 
   return reversedNum===n
 }
+
+
+function isArmStrong(n){
+  let number = n
+  let digitsCubeSum = 0
+  while(number>0){
+    digitsCubeSum+=(number%10)**3
+    number = Math.floor(number/10)
+  }
+  return digitsCubeSum===n
+}
+function isNarcissisticNumber(n){
+  let number = n
+  let digitsCubeSum = 0
+  while(number>0){
+    digitsCubeSum+=(number%10)**4
+    number = Math.floor(number/10)
+  }
+  return digitsCubeSum===n
+}
+
+function sumOfDigits(n){
+  let number = n
+  let digitsSum = 0
+  while(number>0){
+    digitsSum+=number%10
+    number = Math.floor(number/10)
+  }
+  return digitsSum
+}
+
+function averageOfDigits(n){
+  let number = n
+  let digitsSum = 0
+  let totalDigits = 0
+  while(number>0){
+    digitsSum+=number%10
+    totalDigits+=1
+    number = Math.floor(number/10)
+  }
+  return digitsSum/totalDigits
+}
+
+function findSmallestAndLargestDigit(n){
+  let number = n
+  let largestDigits = -Infinity
+  let smallestDigits = Infinity
+  while(number>0){
+    if(number%10>largestDigits){
+      largestDigits=number%10
+    }
+    if (number%10<smallestDigits){
+      smallestDigits=number%10
+    }
+    number = Math.floor(number/10)
+  }
+  return {largestDigits,smallestDigits}
+}
+
+function isStrongNum(n){
+  let number = n
+  let eachDigitsFactorial = number%10
+  let eachDigitsFactorialSum = 0
+  while(number>0){
+    for(let i = number%10; i>1; i--){
+      eachDigitsFactorial*=(i-1)
+    }
+    eachDigitsFactorialSum+=eachDigitsFactorial
+    
+    number = Math.floor(number/10)
+    eachDigitsFactorial = number % 10;
+  }
+  return eachDigitsFactorialSum === n
+}
+
+
+
+
+
+
+
+
+
