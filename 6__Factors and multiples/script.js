9873f3fd7e621c3486e0e579763bb55f8dc77450
krishna <krishna.yadav23x@gmail.com>
2026-09-13 19:35:09 +0530

    HCF

--- 6__Factors and multiples/script.js
+++ 6__Factors and multiples/script.js
@@ -1,44 +1,41 @@
 function printAllFactors(n) {
   const output = [];
-  let count = 0
-  for (let i = 1; i <=Math.floor(Math.sqrt(n)); i++) {
-    count++
+  let count = 0;
+  for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++) {
+    count++;
     if (n % i === 0) {
       // output.push(i);
-      output.splice(output.length/2,0,i)
-      if(i!==n/i){
+      output.splice(output.length / 2, 0, i);
+      if (i !== n / i) {
         // output.push(n/i)
-        output.splice(output.length / 2+1, 0, n/i);
+        output.splice(output.length / 2 + 1, 0, n / i);
       }
     }
   }
   console.log(count);
-  return output
+  return output;
 }
 
-function printAllMultiplesOfNumber(n,limit){
-  let output = []
-  for(let i=n; i<=limit; i+=n){
-    output.push(i)
+function printAllMultiplesOfNumber(n, limit) {
+  let output = [];
+  for (let i = n; i <= limit; i += n) {
+    output.push(i);
   }
-  
-  return output
+
+  return output;
 }
 
+function findHCF(n1, n2) {
+  let small = n1 < n2 ? n1 : n2;
+  let large = n1 > n2 ? n1 : n2;
 
-// function findHCF(num1,num2){
-//   const largeNum = num1>num2?num1:num2
-//   const smaller = num1<num2?num1:num2
-//   let remainder 
-//   while()
-// }
-
-
-
-
-
-
-
+  while (large % small !== 0) {
+    let remainder = large % small;
+    large = small;
+    small = remainder;
+  }
+  return small
+}
 
 
 
